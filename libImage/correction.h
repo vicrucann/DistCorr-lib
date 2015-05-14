929138eb2c5df18609657f643fef97bd766cb1fc
vicrucann <vicrucann@users.noreply.github.com>
2015-05-14 11:55:27 -0400

    comment update

--- libImage/correction.h
+++ libImage/correction.h
@@ -1,6 +1,6 @@
 /* Correct image optical distortion: based on obtained bicubic
 polynomial.
-    Copyright (C) 2014 Victoria Rudakova <vicrucann@gmail.com>
+    Copyright (C) 2014 <vicrucann@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
