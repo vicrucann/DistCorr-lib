5f9ba24e6ad73b77b309f532dce90fafcda68a17
vicrucann <vicrucann@users.noreply.github.com>
2015-05-14 11:58:30 -0400

    Update distortion.cpp

--- libDistortion/distortion.cpp
+++ libDistortion/distortion.cpp
@@ -1,6 +1,6 @@
 /* Bicubic distortion model: obtain using Levenberg-Marquardt 
 minimization.
-    Copyright (C) 2014 Victoria Rudakova <vicrucann@gmail.com>
+    Copyright (C) 2014 <vicrucann@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
